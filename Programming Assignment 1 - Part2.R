8996e674e191f5dc740f2ba0d396fb742969d97d
Teo Lo Lo Piparo <36920983+Teolone88@users.noreply.github.com>
2020-09-08 17:49:54 +0100

    Update Programming Assignment 1 - Part2.R

--- Programming Assignment 1 - Part2.R
+++ Programming Assignment 1 - Part2.R
@@ -5,7 +5,7 @@ complete <- function(directory, id = 1:332) {
     list <-list.files(wd,all.files = TRUE)
     ## Create new tmp dataframe
     df <- data.frame(id=NA, nobs=NA)
-    ##Loop to index IDs
+    ##Loop to index vector IDs
     for (i in 1:length(id)) {
         df[i,1] <- id[i]
         df[i,2] <- sum(complete.cases(read.csv(list[id[i]])))
